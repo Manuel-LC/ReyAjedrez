3fe6f250e6e32cef4e0d9ecef4ec882d59a8622e
Manuel-LC <mlc280803@gmail.com>
2023-11-27 11:22:59 +0100

    Creado el enum llamado Direccion con sus literales.

---
+++ src/main/java/org/iesalandalus/programacion/reyajedrez/Direccion.java
@@ -0,0 +1,14 @@
+package org.iesalandalus.programacion.reyajedrez;
+
+public enum Direccion {
+    NORTE,
+    NORESTE,
+    ESTE,
+    SURESTE,
+    SUR,
+    SUROESTE,
+    OESTE,
+    NOROESTE,
+    ENROQUE_CORTO,
+    ENROQUE_LARGO
+}
